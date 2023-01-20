6c7a21955a57e5e37d96e67c2ac608f291414c10
Arikaim <info@arikaim.com>
2023-01-20 20:17:14 +0200

    =

---
+++ Ai.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Arikaim
+ *
+ * @link        http://www.arikaim.com
+ * @copyright   Copyright (c)  Konstantin Atanasov <info@arikaim.com>
+ * @license     http://www.arikaim.com/license
+ * 
+*/
+namespace Arikaim\Modules\Ai;
+
+use Arikaim\Core\Extension\Module;
+
+/**
+ * Ai module class
+ */
+class Ai extends Module
+{   
+    /**
+     * Install module
+     *
+     * @return void
+     */
+    public function install()
+    {
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+## Arikaim CMS AI Module
+![1.0.0](https://img.shields.io/github/release/arikaim-ai/ai-module.svg)
+![GPLv3](https://img.shields.io/badge/License-GPLv3-blue.svg)
+
+
+Arikaim CMS AI module
+
+
+#### Requirements  
+  * [Arikaim CMS](https://github.com/arikaim/arikaim)
+
+
+
+#### Installation
+
+```sh
+
+composer require arikaim/ai-module
+
+```
